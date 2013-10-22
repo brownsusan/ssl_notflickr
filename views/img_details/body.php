9913fd3d003aae55efd4e7c5091daee2aed581c1
Susan Brown <brownsusan@fullsail.edu>
2013-10-22 11:28:13 -0400

    style landing

--- views/img_details/body.php
+++ views/img_details/body.php
@@ -1,7 +1,7 @@
-<div>
-	<div><!-- HOLD IMAGE --></div>
-	<span><!-- HOLD IMAGE TITLE --></span>
-	<span><!-- HOLD IMAGE DESC --></span>
-	<a><!-- HOLD IMAGE EDIT --></a>
-	<a><!-- HOLD IMAGE DELETE --></a>
+<div id="img_details">
+	<div id="img"><!-- HOLD IMAGE --></div>
+	<span id="img_title">HOLD IMAGE TITLE</span>
+	<span id="img_desc">HOLD IMAGE DESC</span>
+	<a href="#">EDIT</a>
+	<a href="#">DELETE</a>
 </div>
