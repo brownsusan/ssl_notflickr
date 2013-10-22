f953578df6c12e69e57e9b94de3a5fb8f374492b
Susan Brown <brownsusan@fullsail.edu>
2013-10-22 12:10:12 -0400

    style landing

--- views/img_details/body.php
+++ views/img_details/body.php
@@ -1,39 +1,12 @@
-<html>
-	<head>
-		<title>Not Flickr</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
-		<?php
-		// we must load the css from the root (/) because
-		//relative paths don't work with pretty urls and a shared header file'
-		?>
-		<link rel="stylesheet" href="/css/main.css">
-	</head>
-	<body>
-		
-		<div id="header_placeholder"></div>
-		
-		<header>
-<!-- 			TODO CHANGE THE HREF TO REPRESENT THE CONTROLLER THAT HANDLES THE UPLOAD/HOME PAGE -->
-			<a id="logo" href="<?php echo $GLOBALS['web_root']; ?>?action=notes">Not Flicker</a>
-			
-<!-- 			TODO CHANGE THE HREF TO REPRESENT THE CONTROLLER THAT HANDLES THE LOGGING IN -->
-		<form action="<?php echo $GLOBALS['web_root']; ?>?action=submitLogin" method="post" class="home-login" name="home-login">
-			<input name="username" id="loginEmail" placeholder="Username"/>
-			<input name="password" type="password" id="loginPass" placeholder="Password"/>
-			<input type="submit" value="log in" />
-		</form>
-		</header>
-
-
-
-
-
 <div id="img_details">
 	<div id="img"><!-- HOLD IMAGE --></div><br>
 	<span id="img_title">HOLD IMAGE TITLE</span><br>
 	<span id="img_desc">HOLD IMAGE DESC</span><br>
+<!-- 	SET ACTIONS IN THE ANCHORS -->
 	<a href="#">EDIT</a><br>
 	<a href="#">DELETE</a><br>
+</div>
+
+
+<div id="user_photos">
 </div>
