95cc08342ad630acec56996ba7eb8d60d694d67e
Susan Brown <brownsusan@fullsail.edu>
2013-10-22 10:38:45 -0400

    adding content to view files

---
+++ views/img_details/body.php
@@ -0,0 +1,7 @@
+<div>
+	<div><!-- HOLD IMAGE --></div>
+	<span><!-- HOLD IMAGE TITLE --></span>
+	<span><!-- HOLD IMAGE DESC --></span>
+	<a><!-- HOLD IMAGE EDIT --></a>
+	<a><!-- HOLD IMAGE DELETE --></a>
+</div>
---
+++ views/img_update/body.php
@@ -0,0 +1,7 @@
+<div>
+	<div><!-- HOLD IMAGE --></div>
+	<input type="text" value=""><!-- HOLD IMAGE TITLE -->
+	<input type="textarea" value=""><!-- HOLD IMAGE DESC -->
+	<a><!-- HOLD IMAGE SAVE --></a>
+	<a><!-- HOLD IMAGE DELETE --></a>
+</div>
