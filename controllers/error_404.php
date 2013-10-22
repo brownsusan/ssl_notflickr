9e1a5690e012df7b679952b93ed8f194f68c35c0
Susan Brown <brownsusan@fullsail.edu>
2013-10-22 12:20:10 -0400

    added 404 controller

---
+++ controllers/error_404.php
@@ -0,0 +1,7 @@
+<?php
+
+$viewModel = new viewModel();
+$viewModel -> getView('views/template/header.php');
+$viewModel -> getView('views/error_404/body.php');
+$viewModel -> getView('views/template/foot.php');
+
