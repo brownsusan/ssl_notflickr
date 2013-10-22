adccde4b21ad1a8bf33c6846d1f6745078fa9503
Susan Brown <brownsusan@fullsail.edu>
2013-10-22 10:31:02 -0400

    adding content to view files

--- views/img_upload/upload.php
+++ views/img_upload/upload.php
@@ -1,5 +1,7 @@
 <div class="upload">
 	<input type="file" name="upload_file" id="upload_file" placeholder="Select your image(s)...">
-	<input type="text" name="upload_title" id="uoload_title" placeholder="Enter Image Title...">
+	<input type="text" name="upload_title" id="upload_title" placeholder="Enter Image Title...">
+	<input type="textarea" name="upload_desc" id="upload_desc" placeholder="Enter Image Description...">
+
 	<input type="submit" id="upload_submit" value="Upload">
 </div>
