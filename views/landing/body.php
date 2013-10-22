f9d2abde4250863cc81ff56149d3e3cb551339cd
Susan Brown <brownsusan@fullsail.edu>
2013-10-22 11:19:24 -0400

    style landing

--- views/landing/body.php
+++ views/landing/body.php
@@ -11,38 +11,39 @@
 
 
 
-<!-- add in side div -->
-<form action="<?php echo $GLOBALS['web_root']; ?>?action=action_user_create" method="post">
-		<div>
-			<label for="firstname">First Name</label>
-			<input name="firstname" id="firstName" type="text" placeholder="ex: John">
-		</div>
-		<div>
-			<label for="lastname">Last Name</label>
-			<input name="lastname" id="lastName" type="text" placeholder="ex: Doe">
-		</div>
-		<div>
-			<label for="username">Username</label>
-			<input name="username" id="username" type="text" placeholder="ex: johndoe1234">
-		</div>
-		<div>
-			<label for="email">Email Address</label>
-			<input name="email" id="email" type="email" placeholder="ex: jdoe@domain.com">
-		</div>
-		<div>
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password" type="text">
-		</div>
-		<div>
-			<label for="confirmPassword">Confirm Password</label>
-			<input type="password" name="confirmPassword" id="confirmPassword" type="text">
-		</div>
-		<div>
-			<input id="terms" name="terms" value="I agree to the terms and conditions." type="checkbox">
-			<label for="terms">I agree to the terms and conditions.</label>
-		</div>
-		<button "sign_up" class="button-submit home-sign-up">
-			Sign Up
-		</button>
-		
-</form>
+<div id="signup">
+	<form action="<?php echo $GLOBALS['web_root']; ?>?action=action_user_create" method="post">
+			<div>
+				<label for="firstname">First Name</label>
+				<input name="firstname" id="firstName" type="text" placeholder="ex: John">
+			</div>
+			<div>
+				<label for="lastname">Last Name</label>
+				<input name="lastname" id="lastName" type="text" placeholder="ex: Doe">
+			</div>
+			<div>
+				<label for="username">Username</label>
+				<input name="username" id="username" type="text" placeholder="ex: johndoe1234">
+			</div>
+			<div>
+				<label for="email">Email Address</label>
+				<input name="email" id="email" type="email" placeholder="ex: jdoe@domain.com">
+			</div>
+			<div>
+				<label for="password">Password</label>
+				<input type="password" name="password" id="password" type="text">
+			</div>
+			<div>
+				<label for="confirmPassword">Confirm Password</label>
+				<input type="password" name="confirmPassword" id="confirmPassword" type="text">
+			</div>
+			<div>
+				<input id="terms" name="terms" value="I agree to the terms and conditions." type="checkbox">
+				<span>I agree to the terms and conditions.</span>
+			</div>
+			<button "sign_up" class="button-submit home-sign-up">
+				Sign Up
+			</button>
+			
+	</form>
+</div>
